8f6e08fcbc2bfa5fe895c25925d401b90a91372c
takl95 <laszlotakacs.95+git@gmail.com>
2017-07-20 14:01:08 +0200

    backend view structure

---
+++ resources/views/backend/home.blade.php
@@ -0,0 +1,17 @@
+@extends('backend.layouts.main')
+
+@push('title')
+Dashboard
+@endpush
+
+
+@push('content')
+Welcome Home
+@endpush
+
+@push('extrajs')
+<script>
+	alert('welcome home');
+</script>
+@endpush
+
