07bb9b8b4aae83d3ce037b07f04e0007a9eae581
takl95 <laszlotakacs.95+git@gmail.com>
2017-07-20 12:31:58 +0200

    l54 installed

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
+        if(env('APP_ENV',"production") !== 'local')
+        {
+            $url->forceScheme('https');
+        }
     }
 
     /**
