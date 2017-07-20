a9195be6c86995f8eb7817169f13fb86de5c9e66
takl95 <laszlotakacs.95+git@gmail.com>
2017-07-20 14:17:41 +0200

    Backend init
    no styling done

--- resources/views/backend/home.blade.php
+++ resources/views/backend/home.blade.php
@@ -6,12 +6,36 @@ Dashboard
 
 
 @push('content')
-Welcome Home
+<h1>
+    Welcome to {{ config('app.name', 'Laravel') }}
+</h1>
+<h2 class="subheader">
+    You are logged in als {{ Auth::user()->name }}
+</h2>
+
+<br />
+
+<ul>
+	<li><a href="{{ route('home') }} ">Dashboard</a></li>
+	<li><a href="{{ url('manage/users') }} ">Users</a></li>
+	<li><a href="{{ url('') }} ">Asperiores</a></li>
+	<li><a href="{{ url('') }} ">Nam</a></li>
+	<li><a href="{{ url('') }} ">Aspernatur</a></li>
+	<li><a href="{{ url('') }} ">Veniam</a></li>
+</ul>
+
+<br/>
+<a href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+    Logout
+</a>
+<form action="{{ route('logout') }}" id="logout-form" method="POST" style="display: none;">
+    {{ csrf_field() }}
+</form>
 @endpush
 
 @push('extrajs')
 <script>
-	alert('welcome home');
+    alert('welcome home');
 </script>
 @endpush
-
