1f4d849d7e4b720db80c7e6d288641f027007307
takl95 <laszlotakacs.95+git@gmail.com>
2017-07-22 20:23:11 +0200

    cfg test fix test

--- app/Http/Controllers/ManageController.php
+++ app/Http/Controllers/ManageController.php
@@ -8,6 +8,7 @@ class ManageController extends Controller
 {
     public function index()
     {
+    	dump(config('APP_NAME'));
         config(['APP_NAME' => "Program based"]);
         dd(config('APP_NAME'));
 
