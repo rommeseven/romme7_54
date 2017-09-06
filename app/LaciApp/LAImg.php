244364f2ec760a9266b057e611d5c2f81225a6ef
takl95 <laszlotakacs.95+git@gmail.com>
2017-09-06 15:56:44 +0200

    laimg fix 4

--- app/LaciApp/LAImg.php
+++ app/LaciApp/LAImg.php
@@ -12,7 +12,7 @@ class LAImg
         }
         else
         {
-            return Storage::getDriver()->getAdapter()->getClient()->getTemporaryLink($path);
+            return Storage::getDriver()->getAdapter()->getTemporaryLink($path);
         }
     }
 }
