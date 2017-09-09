d5000d385dd38df3f1bac331f2089f34224d6801
takl95 <laszlotakacs.95+git@gmail.com>
2017-09-09 19:51:06 +0200

    deploy script fix

--- herokuinstall.bat
+++ herokuinstall.bat
@@ -3,11 +3,12 @@ call heroku config:set APP_LOG=errorlog
 call heroku config:set CACHE_DRIVER=database
 call heroku config:set SESSION_DRIVER=database
 call heroku config:set APP_DEBUG=true
+call heroku config:set DB_CONNECTION=pgsql
 call heroku config:set MAIL_DRIVER=smtp
 call heroku config:set MAIL_HOST=smtp.gmail.com
 call heroku config:set MAIL_PORT=465
-call heroku config:set MAIL_USERNAME=laszlotakacs.95@gmail.com
-call heroku config:set MAIL_PASSWORD=eTL95+MyWish2187
+call heroku config:set MAIL_USERNAME=sevenwebagentur@gmail.com
+call heroku config:set MAIL_PASSWORD=Dencanwait1998
 call heroku config:set MAIL_ENCRYPTION=ssl
 call heroku config:set MAIL_FROM_ADDRESS=laszlotakacs.95+seven@gmail.com
 call heroku config:set MAIL_FROM_NAME="Christian Neuherz - SEVEN Webagentur"
@@ -18,4 +19,4 @@ call heroku addons:create mailgun:starter
 call git push heroku master
 call heroku run php artisan migrate
 call heroku run php artisan cms:install
-
+call heroku open
