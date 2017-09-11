c256ec3bea6f8d30689d3cafeb72f86cdb0d0906
takl95 <laszlotakacs.95+git@gmail.com>
2017-09-11 11:39:04 +0200

    lang test
    works
    
    json + php files together

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -5,6 +5,11 @@
 	<title>Document</title>
 </head>
 <body>
+
+@lang("I love programming.")
+<br />
+@lang("dashboard.info")
+
 	Hello
 	<img src="{{ Cloudder::secureShow("sample") }}" alt="" />
 </body>
