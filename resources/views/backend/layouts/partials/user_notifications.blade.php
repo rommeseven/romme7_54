794ba6c475fd0c3e8966372babd2929002e749fc
takl95 <laszlotakacs.95+git@gmail.com>
2017-09-12 20:35:54 +0200

    todo review

--- resources/views/backend/layouts/partials/user_notifications.blade.php
+++ resources/views/backend/layouts/partials/user_notifications.blade.php
@@ -2,67 +2,130 @@
     <div class="column expand">
         <div class="callout alert-callout-subtle success unread radius flex-container align-justify">
             <div class="notif-text">
-               <strong><a href="#" title="See Details">takl95</a></strong> updated the Application to <strong><a href="#" title="See Details">v12</a></strong>! 
+                <strong>
+                    <a href="#" title="See Details">
+                        takl95
+                    </a>
+                </strong>
+                updated the Application to
+                <strong>
+                    <a href="#" title="See Details">
+                        v12
+                    </a>
+                </strong>
+                !
             </div>
             <!-- END OF .notif-text -->
             <div class="notif-icons">
-                <a href=""><i class="fa fa-trash">
-                    </i></a>
+                <a href="">
+                    <i class="fa fa-trash">
+                    </i>
+                </a>
             </div>
             <!-- END OF .notif-icons -->
         </div>
         <div class="callout alert-callout-subtle alert unread flex-container align-justify" title="Mark as Read">
             <div class="notif-text">
-               There is a new <strong><a href="#" title="See Details">Error Report</a></strong>!
+                There is a new
+                <strong>
+                    <a href="#" title="See Details">
+                        Error Report
+                    </a>
+                </strong>
+                !
             </div>
             <!-- END OF .notif-text -->
             <div class="notif-icons">
-                <a href="" title="Delete Notification"><i class="fa fa-trash">
-                    </i></a>
+                <a href="" title="Delete Notification">
+                    <i class="fa fa-trash">
+                    </i>
+                </a>
             </div>
             <!-- END OF .notif-icons -->
         </div>
         <div class="callout alert-callout-subtle primary unread radius flex-container align-justify">
-              <div class="notif-text">
-               <strong><a href="#" title="See Details">takl95</a></strong> created a <strong><a href="#" title="See Details">new Page</a></strong>!
+            <div class="notif-text">
+                <strong>
+                    <a href="#" title="See Details">
+                        takl95
+                    </a>
+                </strong>
+                created a
+                <strong>
+                    <a href="#" title="See Details">
+                        new Page
+                    </a>
+                </strong>
+                !
             </div>
             <!-- END OF .notif-text -->
             <div class="notif-icons">
-                <a href=""><i class="fa fa-trash">
-                    </i></a>
+                <a href="">
+                    <i class="fa fa-trash">
+                    </i>
+                </a>
             </div>
             <!-- END OF .notif-icons -->
         </div>
         <div class="callout alert-callout-subtle warning read small flex-container align-justify">
             <div class="notif-text">
-               There are less then <strong><a href="#" title="See Details">3 pages</a></strong> at the moment on the site.
+                There are less then
+                <strong>
+                    <a href="#" title="See Details">
+                        3 pages
+                    </a>
+                </strong>
+                at the moment on the site.
             </div>
             <!-- END OF .notif-text -->
             <div class="notif-icons">
-                <a href=""><i class="fa fa-trash">
-                    </i></a>
+                <a href="">
+                    <i class="fa fa-trash">
+                    </i>
+                </a>
             </div>
             <!-- END OF .notif-icons -->
         </div>
         <div class="callout alert-callout-subtle alert read small flex-container align-justify">
             <div class="notif-text">
-               There is a new <strong><a href="#" title="See Details">Error Report</a></strong>!
+                There is a new
+                <strong>
+                    <a href="#" title="See Details">
+                        Error Report
+                    </a>
+                </strong>
+                !
             </div>
             <!-- END OF .notif-text -->
             <div class="notif-icons">
-                <a href=""><i class="fa fa-trash">
-                    </i></a>
+                <a href="">
+                    <i class="fa fa-trash">
+                    </i>
+                </a>
             </div>
             <!-- END OF .notif-icons -->
         </div>
         <div class="callout alert-callout-subtle primary read small flex-container align-justify">
             <div class="notif-text">
-               <strong><a href="#" title="See Details">takl95</a></strong> updated the Application to <strong><a href="#" title="See Details">v12</a></strong>! 
+                <strong>
+                    <a href="#" title="See Details">
+                        takl95
+                    </a>
+                </strong>
+                updated the Application to
+                <strong>
+                    <a href="#" title="See Details">
+                        v12
+                    </a>
+                </strong>
+                !
             </div>
             <!-- END OF .notif-text -->
             <div class="notif-icons">
-                <a href=""><i class="fa fa-trash">
-                    </i></a>
+                <a href="">
+                    <i class="fa fa-trash">
+                    </i>
+                </a>
             </div>
             <!-- END OF .notif-icons -->
         </div>
@@ -70,3 +133,5 @@
     <!-- END OF .column expand -->
 </div>
 <!-- END OF .row -->
+{{-- TODO: [notifs] add timestamp (ago) --}}
+{{-- TODO: [notifs] make dynamic --}}
