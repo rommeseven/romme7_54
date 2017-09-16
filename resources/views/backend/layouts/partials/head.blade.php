6c70af3770169764b13461e4c6b11b73a32c71a1
takl95 <laszlotakacs.95+git@gmail.com>
2017-09-16 15:20:43 +0200

    backend favicon fix

--- resources/views/backend/layouts/partials/head.blade.php
+++ resources/views/backend/layouts/partials/head.blade.php
@@ -6,8 +6,8 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         {{-- //TODO: Add app favicon  @dropbox @internet --}}
-        <link rel="shortcut icon" href="asset('img/seven.ico')" type="image/x-icon">
-        <link rel="icon" href="asset('cms/img/seven.ico')" type="image/x-icon">
+        <link rel="shortcut icon" href="{{asset('img/seven.ico')}}" type="image/x-icon">
+        <link rel="icon" href="{{asset('img/seven.ico')}}" type="image/x-icon">
         <title>{{ settings('app_title') }} | @stack('title')</title>
         <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Biryani" rel="stylesheet">        
