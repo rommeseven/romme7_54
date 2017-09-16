fef29b0a436fd0366d85862112e40f43bc1fd839
takl95 <laszlotakacs.95+git@gmail.com>
2017-09-16 13:54:58 +0200

    cdn only on prod

--- resources/views/backend/pages/step2.blade.php
+++ resources/views/backend/pages/step2.blade.php
@@ -200,8 +200,15 @@ let app = new Vue(
 </script>
 @endpush
 @push('extracss')
+
+ @if(App::environment('local'))
+
+<link href="{{asset('othet/vendor/jquery-ui.min.css')}}" rel="stylesheet" type="text/css">
+ @else
 <link href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css" rel="stylesheet" type="text/css">
+@endif
 {{-- TODO: @online ADD LOCAL css for local env --}}
+
 
 @endpush
 
@@ -215,9 +222,16 @@ let app = new Vue(
 
 
 @push('extrajs')
-{{-- TODO: @online ADD LOCAL css for local env --}}
+
+ @if(App::environment('local'))
+<script src="{{asset('othet/vendor/nestedSortable.js')}}"></script>
+ @else
 <script src="https://cdnjs.cloudflare.com/ajax/libs/nestedSortable/2.0.0/jquery.mjs.nestedSortable.min.js">
 </script>
+@endif
+
+
+{{-- TODO: @online ADD LOCAL css for local env --}}
 <script>
 
 $(function()
