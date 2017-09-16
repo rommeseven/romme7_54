132526565a970ff21c45de935d2085d45c1a64d4
takl95 <laszlotakacs.95+git@gmail.com>
2017-09-16 19:18:09 +0200

    landing page title fix

--- resources/views/frontend/modules/landing-page.blade.php
+++ resources/views/frontend/modules/landing-page.blade.php
@@ -1,4 +1,28 @@
-@include("frontend.partials.head")
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    {{-- //REMEMBER: Add public favicon --}}
+        <link rel="shortcut icon" href="{{asset('img/seven.ico')}}" type="image/x-icon">
+        <link rel="icon" href="{{asset('img/seven.ico')}}" type="image/x-icon">
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Biryani" rel="stylesheet">        
+
+        <title>{{ settings('app_title') }} - {{$page->title}}</title>
+        <link href="{{ mix('css/public.css') }}" rel="stylesheet" type="text/css">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        @stack("extracss")
+    </head>
+    <body>
+
+
+
+
 
 {{-- CRISI: [landing-page] Text (Weiß) umschreiben, gleich auf Deutsch, brauchst nicht übersetzen, ist front-end --}}
 
