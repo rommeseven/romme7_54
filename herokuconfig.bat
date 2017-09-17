8a267d7ef7a38ea5c3a071af35e9ab55626410c3
takl95 <laszlotakacs.95+git@gmail.com>
2017-09-17 17:49:52 +0200

    heroku cfg update

--- herokuconfig.bat
+++ herokuconfig.bat
@@ -5,8 +5,10 @@ call heroku config:set SESSION_DRIVER=database
 call heroku config:set APP_DEBUG=true
 call heroku config:set DB_CONNECTION=pgsql
 call heroku config:set MAIL_DRIVER=smtp
-call heroku config:set MAIL_HOST=smtp.sendgrid.net
-call heroku config:set MAIL_PORT=587
+rem call heroku config:set MAIL_HOST=smtp.sendgrid.net
+rem call heroku config:set MAIL_PORT=587   rem SENGDRID
+call heroku config:set MAIL_HOST=smtp.gmail.com
+call heroku config:set MAIL_PORT=465 rem GMAIL
 call heroku config:set MAIL_ENCRYPTION=ssl
 call heroku config:set MAIL_FROM_ADDRESS=noreply@sevenweb.eu
 call heroku config:set MAIL_FROM_NAME="SEVEN Webagentur"
@@ -17,6 +19,12 @@ call heroku config:set DBOX_APP_KEY=uc2v7iq2qk243ba
 call heroku config:set DBOX_TOKEN=MQAa74FtCyAAAAAAAAAAEB2JmhgitlJMQYE9mYCWba6tCjRG7RV57tp1-VJBZq1N
 call set /p apptitle="App TITLE: "
 call heroku config:set DEFAULT_APP_TITLE="%apptitle%"
+call set /p appdomain="App DOMAIN: "
+call heroku config:set APP_DOMAIN="%appdomain%"
+call set /p gmailu="Business-GMAIL-Username: "
+call heroku config:set GMAIL_USERNAME="%gmailu%"
+call set /p gmailpw="Business-GMAIL-Password: "
+call heroku config:set GMAIL_PASSWORD="%gmailpw%"
 call heroku addons:create cloudinary:starter
 call heroku addons:create heroku-postgresql:hobby-dev
 call heroku addons:create trevor:hobby
