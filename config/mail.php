21927ed20d58b3be8956553fa896aca671ef97c1
takl95 <laszlotakacs.95+git@gmail.com>
2017-09-17 18:02:26 +0200

    gmail config

--- config/mail.php
+++ config/mail.php
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'smtp.sendgrid.net'),
+    'host' => env('MAIL_HOST', 'smtp.gmail.com'),
 
     /*
     |--------------------------------------------------------------------------
@@ -42,7 +42,7 @@ return [
     |
     */
 
-    'port' => env('MAIL_PORT', 587),
+    'port' => env('MAIL_PORT', 465),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'info@email.sevenweb.eu'),
-        'name' => env('MAIL_FROM_NAME', 'Seven Webagentur'),
+        'address' => env('MAIL_FROM_ADDRESS', 'noreply@' . env('APP_DOMAIN')),
+        'name' => env('MAIL_FROM_NAME',env('DEFAULT_APP_TITLE')),
     ],
 
     /*
@@ -71,7 +71,7 @@ return [
     |
     */
 
-    'encryption' => env('MAIL_ENCRYPTION', 'tls'),
+    'encryption' => env('MAIL_ENCRYPTION', 'ssl'),
 
     /*
     |--------------------------------------------------------------------------
@@ -84,9 +84,9 @@ return [
     |
     */
 
-    'username' => env('SENDGRID_USERNAME'),
+    'username' => env('GMAIL_USERNAME'),
 
-    'password' => env('SENDGRID_PASSWORD'),
+    'password' => env('GMAIL_PASSWORD'),
 
     /*
     |--------------------------------------------------------------------------
