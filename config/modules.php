9310fcfdad85ce7441d819f139e0f9ff4c361073
takl95 <laszlotakacs.95+git@gmail.com>
2017-09-24 10:58:49 +0200

    Revert "modules php @lang"
    This reverts commit 34a14ff0ad5924b5e99e78f4da915739d174712c.

--- config/modules.php
+++ config/modules.php
@@ -3,12 +3,13 @@ return array(
     'app'   => env("APP_CODE", "seven"),
 
     'seven' => array(
-        array('view' => 'landing-page', 'name' => '@lang("Landing Page")',
+
+        array('view' => 'landing-page', 'name' => "Landing Page",
         ),
-        array('view' => 'placeholder', 'name' => '@lang("Placeholder")',
+        array('view' => 'placeholder', 'name' => "Placeholder",
         ),
     ),
-
+// CRISI: @lang  unter 'seven' die 'name ' variablen übersetzen
     'vogl'  => array(
         /* ANDERE MODULES */
     ),
